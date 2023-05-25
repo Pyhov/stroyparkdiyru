acf63a872d235dbae24c39b5136d425aa8df1ca0
Алексей <apyhov98@gmail.com>
2023-05-25 10:05:59 +0700

    add GetRequest.cs

--- stroyparkdiyru/GetRequest.cs
+++ stroyparkdiyru/GetRequest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,36 @@ namespace stroyparkdiyru
 {
     internal class GetRequest
     {
-    }
+
+            string BaseAddress = "https://stroyparkdiy.ru/";
+            public string Response { get; set; }
+            public async Task RunAsync(string Url)
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(BaseAddress);
+                    client.DefaultRequestHeaders.Add("method", "GET");
+
+                    var responce = await client.GetAsync(Url);
+                    try
+                    {
+                        if (responce.StatusCode == HttpStatusCode.OK)
+                        {
+                            Response = await responce.Content.ReadAsStringAsync();
+                        }
+                        else
+                        {
+                            throw new Exception(responce.StatusCode.ToString());
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.Print(e.Message);
+                        Response = string.Empty;
+                    }
+
+                }
+            }
+        }
+    
 }
